9faa15d4f6645362f920f411d37780071bc7f5f0
Dlesserko <113294746+Dlesserko@users.noreply.github.com>
2022-12-04 17:50:25 +0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,11 @@
+TOKEN = "5877314906:AAH9_oyOwWqtCEqnl7XgLVCxC4wtzkBQ2fY"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'тенге': 'KZT',
+    'юань': 'CNY'
+}
